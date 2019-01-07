71e90167ed6b305ec0ed10f00724fbf56f11d500
Vitor Dino <vitor@kunstdesign.com.br>
2019-01-06 22:32:48 -0200

    :sparkles: add CurrentTime context

---
+++ src/contexts/CurentTime.js
@@ -0,0 +1,20 @@
+import React, {createContext, useContext, useState, useEffect} from 'react'
+
+const initialState = Date.now()
+const Context = createContext(initialState)
+
+export const Provider = ({children}) => {
+	const [currentTime, setCurrentTime] = useState(initialState)
+
+	useEffect(() => {
+		const interval = setInterval(() => setCurrentTime(Date.now()), 1000)
+		return () => clearInterval(interval)
+	}, [])
+
+	return (
+		<Context.Provider value={currentTime}>{children}</Context.Provider>
+	)
+}
+
+export default Context
+export const useCurrentTime = () => useContext(Context)
