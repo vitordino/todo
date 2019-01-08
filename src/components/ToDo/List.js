f518799da08ef1d30c231b71d68a8fa19544e0c9
Vitor Dino <vitor@kunstdesign.com.br>
2019-01-08 17:46:32 -0200

    :construction: List alerts

--- src/components/ToDo/List.js
+++ src/components/ToDo/List.js
@@ -1,7 +1,10 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useEffect } from 'react'
 import { useAuthState, useList } from '../../utils/firebase-hooks'
+import { addMinutes, differenceInMinutes } from 'date-fns'
+import { withAlert } from 'react-alert'
 import { useInputState } from '../../utils/hooks'
 import { toArray } from '../../utils/firebase'
+import { useCurrentTime } from '../../contexts/CurentTime'
 import EmptyState from '../EmptyState'
 import Container from '../Container'
 import Select from '../Select'
@@ -22,12 +25,43 @@ const EmptyList = ({filter = 0}) => (
 	</EmptyState>
 )
 
-const Wrapper = ({render, children = render, ...props}) => {
+const approximatlyEqual = (a, b, factor = 1e4) => (
+	Math.round(a/factor) === Math.round(b/factor)
+)
+const isWithinMinutes = (a, b, minutes = 5) => (
+	Math.abs(differenceInMinutes(a, b)) < minutes
+)
+
+const useTodoAlerts = list => useCurrentTime(currentTime => {
+	const alerts = list.filter(todo => {
+		if(currentTime > todo.dueTime) return false
+		return isWithinMinutes(currentTime, todo.dueTime, 5)
+	})
+	const overdue = list.filter(
+		todo => approximatlyEqual(todo.dueTime, currentTime)
+	)
+	return [alerts, overdue]
+})
+
+const Wrapper = ({render, children = render, alert, ...props}) => {
+	// get list
 	const { uid } = useAuthState()
 	const [sortBy, setSortBy] = useInputState('key')
 	const [filter, setFilter] = useInputState('0')
-	const { loading, value } = useList(`todos/${uid}`, {sortBy})
-	const list = loading ? [] : getFilteredList(filter|0, value)
+	const { loading, value: allTodos } = useList(`todos/${uid}`, {sortBy})
+	const list = loading ? [] : getFilteredList(filter|0, allTodos)
+
+	// manage alerts
+	const [alerts, overdue] = useTodoAlerts(list)
+
+	useEffect(() => {
+		if(alerts.length) alerts.forEach(({title}) => alert.show(`${title} is about to expire`))
+	}, [...alerts.map(a => a.key)])
+
+	useEffect(() => {
+		if(overdue.length) overdue.forEach(({title}) => alert.show(`${title} is overdue`))
+	}, [...overdue.map(a => a.key)])
+
 
 	return (
 		<Container {...props}>
@@ -50,4 +84,4 @@ const List = props => (
 	</Wrapper>
 )
 
-export default List
+export default withAlert(List)
