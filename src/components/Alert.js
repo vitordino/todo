d913e3e4586b241ff15f24a4c2bb61f6cf5059ec
Vitor Dino <vitor@kunstdesign.com.br>
2019-01-08 14:16:36 -0200

    :rotating_light:

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,7 +1,6 @@
 import React, {useEffect} from 'react'
 import styled from 'styled-components'
 import Feather from './Feather'
-import {Paragraph} from './Text'
 
 /* eslint-disable */
 const Wrapper = styled.div`
